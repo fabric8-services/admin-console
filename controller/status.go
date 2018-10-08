dea196f7e65198437e9e2f2efa7bbed3c126bf95
Xavier Coulon <xcoulon@redhat.com>
2018-10-08 12:27:18 +0200

    update dependency on fabric8-common (#4)
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- controller/status.go
+++ controller/status.go
@@ -17,11 +17,6 @@ func NewStatusController(service *goa.Service) *StatusController {
 
 // Show runs the show action.
 func (c *StatusController) Show(ctx *app.ShowStatusContext) error {
-	// StatusController_Show: start_implement
-
-	// Put your logic here
-
-	// StatusController_Show: end_implement
 	res := &app.Status{}
 	return ctx.OK(res)
 }
