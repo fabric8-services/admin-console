7a331b2f6309183b604beb43d0f7efc282a102c6
Xavier Coulon <xcoulon@redhat.com>
2018-10-08 12:30:29 +0200

    build: add support for build on Jenkins
    fixes #2
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

---
+++ cico_setup.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+# Output command before executing
+set -x
+
+# Exit on error
+set -e
+
+# Source environment variables of the jenkins slave
+# that might interest this worker.
+function load_jenkins_vars() {
+  if [ -e "jenkins-env" ]; then
+    cat jenkins-env \
+      | grep -E "(DEVSHIFT_TAG_LEN|QUAY_USERNAME|QUAY_PASSWORD|JENKINS_URL|GIT_BRANCH|GIT_COMMIT|BUILD_NUMBER|ghprbSourceBranch|ghprbActualCommit|BUILD_URL|ghprbPullId)=" \
+      | sed 's/^/export /g' \
+      > ~/.jenkins-env
+    source ~/.jenkins-env
+  fi
+}
+
+function install_deps() {
+  # We need to disable selinux for now, XXX
+  /usr/sbin/setenforce 0 || :
+
+  # Get all the deps in
+  yum -y install \
+    docker \
+    make \
+    git \
+    curl
+
+  service docker start
+
+  echo 'CICO: Dependencies installed'
+}
+
+function cleanup_env {
+  EXIT_CODE=$?
+  echo "CICO: Cleanup environment: Tear down test environment"
+  make integration-test-env-tear-down
+  echo "CICO: Exiting with $EXIT_CODE"
+}
+
+function prepare() {
+  # Let's test
+  make docker-start
+  make docker-check-go-format
+  make docker-deps
+  make docker-generate
+  make docker-build
+  echo 'CICO: Preparation complete'
+}
+
+function run_tests_without_coverage() {
+  make docker-test-unit
+  make integration-test-env-prepare
+  trap cleanup_env EXIT
+
+  # Check that postgresql container is healthy
+  check_postgres_healthiness
+
+  make docker-test-migration
+  make docker-test-integration
+  make docker-test-remote
+  echo "CICO: ran tests without coverage"
+}
+
+function check_postgres_healthiness(){
+  echo "CICO: Waiting for postgresql container to be healthy...";
+  while ! docker ps | grep postgres_integration_test | grep -q healthy; do
+    printf .;
+    sleep 1 ;
+  done;
+  echo "CICO: postgresql container is HEALTHY!";
+}
+
+function run_tests_with_coverage() {
+  # Run the unit tests that generate coverage information
+  make docker-test-unit-with-coverage
+  make integration-test-env-prepare
+  trap cleanup_env EXIT
+
+  # Check that postgresql container is healthy
+  check_postgres_healthiness
+
+  # Run the integration tests that generate coverage information
+  make docker-test-migration
+  make docker-test-integration-with-coverage
+
+  # Run the remote tests that generate coverage information
+  make docker-test-remote-with-coverage
+
+  # Output coverage
+  make docker-coverage-all
+
+  # Upload coverage to codecov.io
+  cp tmp/coverage.mode* coverage.txt
+  bash <(curl -s https://codecov.io/bash) -X search -f coverage.txt -t 8835a743-b3e5-4739-94df-84c145e61f14 #-X fix
+
+  echo "CICO: ran tests and uploaded coverage"
+}
+
+function tag_push() {
+  local tag
+
+  tag=$1
+  docker tag admin-console-deploy $tag
+  docker push $tag
+}
+
+function deploy() {
+  # Login first
+  REGISTRY="quay.io"
+
+  if [ -n "${QUAY_USERNAME}" -a -n "${QUAY_PASSWORD}" ]; then
+    docker login -u ${QUAY_USERNAME} -p ${QUAY_PASSWORD} ${REGISTRY}
+  else
+    echo "Could not login, missing credentials for the registry"
+  fi
+
+  # Build admin-console-deploy
+  make docker-image-deploy
+
+  TAG=$(echo $GIT_COMMIT | cut -c1-${DEVSHIFT_TAG_LEN})
+
+  if [ "$TARGET" = "rhel" ]; then
+    tag_push ${REGISTRY}/openshiftio/rhel-fabric8-services-admin-console:$TAG
+    tag_push ${REGISTRY}/openshiftio/rhel-fabric8-services-admin-console:latest
+  else
+    tag_push ${REGISTRY}/openshiftio/fabric8-services-admin-console:$TAG
+    tag_push ${REGISTRY}/openshiftio/fabric8-services-admin-console:latest
+  fi
+
+  echo 'CICO: Image pushed, ready to update deployed app'
+}
+
+function cico_setup() {
+  load_jenkins_vars;
+  install_deps;
+  prepare;
+}
